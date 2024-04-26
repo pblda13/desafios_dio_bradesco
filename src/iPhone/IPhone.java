412459bf69a09fc8987934045e7848967c57e16c
Pâmela Borges <pamela_borges18@hotmail.com>
2024-04-25 21:29:34 -0300

    Modelagem UML e Implementação do Componente iPhone

---
+++ src/iPhone/IPhone.java
@@ -0,0 +1,47 @@
+
+public class IPhone implements ReprodutorMusical, AparelhoTelefonico, NavegadorInternet {
+    @Override
+    public void tocar() {
+        System.out.println("Tocando música...");
+    }
+
+    @Override
+    public void pausar() {
+        System.out.println("Pausando música...");
+    }
+
+    @Override
+    public void selecionarMusica() {
+        System.out.println("Selecionando música...");
+    }
+
+    @Override
+    public void ligar() {
+        System.out.println("Ligando o telefone...");
+    }
+
+    @Override
+    public void atender() {
+        System.out.println("Atendendo chamada...");
+    }
+
+    @Override
+    public void iniciarCorreioVoz() {
+        System.out.println("Iniciando correio de voz...");
+    }
+
+    @Override
+    public void exibirPagina() {
+        System.out.println("Exibindo página da internet...");
+    }
+
+    @Override
+    public void adicionarNovaAba() {
+        System.out.println("Adicionando nova aba no navegador...");
+    }
+
+    @Override
+    public void atualizarPagina() {
+        System.out.println("Atualizando página no navegador...");
+    }
+}
